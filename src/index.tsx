99999b2ee54c389fc3ebf196dad7ace990689708
Seong Yong-ju <sei40kr@gmail.com>
2020-08-12 00:46:46 +0900

    Replace Parcel with esbuild & sane & serve

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { store } from "./application/store";
-import "./index.css";
 import App from "./presentation/App";
 import * as serviceWorker from "./serviceWorker";
 
